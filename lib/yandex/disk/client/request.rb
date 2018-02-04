7765417d9b6cda924e30ca159fe4ef06611474d9
Marat Galiev <kazanlug@gmail.com>
2018-02-04 14:30:23 +0300

    Publication feature, typo fixes

--- lib/yandex/disk/client/request.rb
+++ lib/yandex/disk/client/request.rb
@@ -1,4 +1,5 @@
 module Yandex::Disk::Client::Request
   autoload :Space, 'yandex/disk/client/request/space'
   autoload :List, 'yandex/disk/client/request/list'
+  autoload :Publication, 'yandex/disk/client/request/publication'
 end
